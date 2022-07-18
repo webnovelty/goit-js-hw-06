df33bf7f0e56b302d8d9565893db12faea8bd1fa
webnovelty <webnoveltystudio@gmail.com>
2022-07-18 23:19:12 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,6 +6,17 @@ const ingredients = [
 	'Herbs',
 	'Condiments',
 ];
-const ingredientsEl = document.querySelector('#ingredients');
 
-console.log(ingredientsEl);
+const ingredientsEl = document.querySelector('#ingredients');
+const elements = [];
+const itemAdd = function (ingredients) {
+	ingredients.forEach(element => {
+		const elementLi = document.createElement('li');
+		elementLi.classList.add('item');
+		elementLi.textContent = element;
+		elements.push(elementLi);
+
+	});
+}
+itemAdd(ingredients);
+ingredientsEl.append(...elements);
