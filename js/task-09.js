b2e4f0c4f8a193aa95dcbf17e9ba29bdeda793ad
webnovelty <webnoveltystudio@gmail.com>
2022-07-21 23:16:23 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+buttonEl.addEventListener('click', onColorChange);
+function onColorChange() {
+	document.body.style.backgroundColor = getRandomHexColor();
+	spanEl.textContent = document.body.style.backgroundColor;
+
 }
