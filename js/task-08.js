5da9c50bd629f98876bc212e4c2bb454f0bb96eb
webnovelty <webnoveltystudio@gmail.com>
2022-07-21 22:41:46 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+const inputEl = document.querySelectorAll('input');
+const submitObj = {};
 
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+	event.preventDefault();
+	const formElements = event.currentTarget.elements;
+	if (formElements.email.value === '' || formElements.password.value === '') {
+		alert('все поля должны быть заполнены')
+	}
+	else {
+		submitObj.email = formElements.email.value;
+		submitObj.password = formElements.password.value;
+		console.log('Объект с данными формы = ', submitObj);
+
+		form.reset();
+
+	}
+
+
+}
