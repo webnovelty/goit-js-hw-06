707edab354cd22f4de22fb140d32f67ec65fef52
webnovelty <webnoveltystudio@gmail.com>
2022-07-22 22:26:05 +0200

    task6v3

--- js/task-06.js
+++ js/task-06.js
@@ -2,8 +2,13 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputValidation);
 function onInputValidation(event) {
 
-	event.currentTarget.value.length <= inputEl.getAttribute('data-length') ?
-		inputEl.classList.add('valid') :
-		inputEl.classList.add('invalid');
 
+	if (event.currentTarget.value.length != inputEl.getAttribute('data-length')) {
+		inputEl.classList.add('invalid');
+		inputEl.classList.remove('valid');
+	}
+	else {
+		inputEl.classList.add('valid');
+		inputEl.classList.remove('invalid');
+	}
 }
