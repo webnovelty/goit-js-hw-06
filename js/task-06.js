64b462c52fc65f1024da54db75c70bdbf4a32b92
webnovelty <webnoveltystudio@gmail.com>
2022-07-23 09:43:08 +0200

    task6fix

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,9 @@
 const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputValidation);
 function onInputValidation(event) {
+	const inputAttribute = Number(inputEl.getAttribute('data-length'));
 
-
-	if (event.currentTarget.value.length != inputEl.getAttribute('data-length')) {
+	if (event.currentTarget.value.length != inputAttribute) {
 		inputEl.classList.add('invalid');
 		inputEl.classList.remove('valid');
 	}
